d070421ce70108aaec050add2a2519d9ba9290f9
sabit <iamsabit99@gmail.com>
2020-05-02 04:40:34 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+var txtomp3 = require("text-to-mp3");
+const fs = require('fs');
+const path = require('path');
+const body = require('body-parser');
+
+const app = express();
+
+let port = process.env.PORT
+
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+
+app.use(body.urlencoded({ extended: true }));
+app.use(body.json());
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+
+app.get('/', (req, res) => {
+  res.render('index');
+
+})
+app.post('/generate', (req, res) => {
+  var msg = req.body.msg;
+
+  // console.log(msg);
+  txtomp3.getMp3(msg, function (err, binaryStream) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    var file = fs.createWriteStream("./public/FileName.mp3"); // write it down the file
+    file.write(binaryStream);
+    file.end();
+
+    res.render("index");
+
+  });
+
+
+})
+
+app.listen(port, () => console.log("Server is Running..."));
